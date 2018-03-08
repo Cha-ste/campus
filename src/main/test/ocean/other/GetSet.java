0eecad7eef1ceebc5f90c6538919e555bee73604
ocean <ocean@hboxs.com>
2018-03-08 09:36:24 +0800

    [ADD] get set

---
+++ src/main/test/ocean/other/GetSet.java
@@ -0,0 +1,20 @@
+package ocean.other;
+
+public class GetSet {
+
+    private String oCean;
+
+    public String getoCean() {
+        return oCean;
+    }
+
+    public void setoCean(String oCean) {
+        this.oCean = oCean;
+    }
+
+    public GetSet(String oCean){
+        this.oCean = oCean;
+    }
+
+
+}
